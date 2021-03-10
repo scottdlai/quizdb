f17ccc79fe7f62dab3bebeca1e2d1e523798dd8c
Scott Lai <tuonglai3602@gmail.com>
2021-03-10 10:53:31 -0800

    Changed ?? to ||.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const questionsRouter = require('./routes/questions');
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

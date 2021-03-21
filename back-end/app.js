69d4db1391d58f91f28b0d52ca278c0ec0d9e044
Scott Lai <tuonglai3602@gmail.com>
2021-03-20 19:57:34 -0700

    added back-end.

---
+++ back-end/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const questionsRouter = require('./routes/questions');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/questions', questionsRouter);
+
+app.listen(PORT, () => console.log(`App listenning on port ${PORT}`));
